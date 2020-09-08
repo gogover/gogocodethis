467e69a4257b0af36bcc24bd6775c42d81db7106
RD <rupindeeplearning@gmail.com>
2020-09-08 02:43:09 -0400

    Initial commit

--- site.js
+++ site.js
@@ -13,6 +13,6 @@
 // limitations under the License.
 
 /** Intentionally empty */
-navigator.serviceWorker && navigator.serviceWorker.register('./sw.js').then(function(registration) {
+navigator.serviceWorker && navigator.serviceWorker.register('sw.js').then(function(registration) {
   console.log('Excellent, registered with scope: ', registration.scope);
 });
