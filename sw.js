7f1bdd08aab2e9ce90b2f569fddbeea844d60d52
RD <rupindeeplearning@gmail.com>
2020-09-08 03:08:34 -0400

    Initial commit

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-self.addEventListener('fetch', function(event) {
+/*self.addEventListener('fetch', function(event) {
 	 
   event.respondWith(
     caches.match(event.request).then(function(response) {
@@ -6,22 +6,44 @@ self.addEventListener('fetch', function(event) {
     })
   );
 });
+*/
+addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;     // if valid response is found in cache return it
+        } else {
+          return fetch(event.request)     //fetch from internet            
+            .catch(function(err) {       // fallback mechanism
+              return caches.open(CACHE_CONTAINING_ERROR_MESSAGES)
+                .then(function(cache) {
+                  return cache.match('/offline.html');
+                });
+            });
+        }
+      })
+  );
+});
+
+
+
 self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open('the-magic-cache').then(function(cache) {
       return cache.addAll([
         '/',
-        'index.html',
-        'dragon.html',
-        'faq.html',
-        'manifest.json',
-        'background.jpeg',
-        'construction.gif',
-        'dragon.png',
-        'logo.png',
-        'site.js',
-        'dragon.js',
-        'styles.css',
+        '/index.html',
+        '/dragon.html',
+        '/faq.html',
+        '/manifest.json',
+        '/background.jpeg',
+        '/construction.gif',
+        '/dragon.png',
+        '/logo.png',
+        '/site.js',
+        '/dragon.js',
+        '/styles.css',
       ]);
     })
   );
